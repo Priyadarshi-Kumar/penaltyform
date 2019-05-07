3ece4d7e6e049cab226c4e47bd978491a778ddb3
Priyadarshi Kumar <priyadarshikumar@Priyadarshis-MacBook-Air.local>
2019-05-07 15:24:27 +0530

    first commit

--- src/PenaltyForm.jsx
+++ src/PenaltyForm.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import MuiPhoneNumber from "material-ui-phone-number"
 
 class PenaltyForm extends Component {
+    handleOnChange = (value) => {
+        this.setState({
+            phone: value
+         });
+    }
   render() {
     return (
       <div>
-        
+        <MuiPhoneNumber defaultCountry={'us'} onChange={this.handleOnChange}/>
       </div>
     )
   }
